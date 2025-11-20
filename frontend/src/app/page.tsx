0eb800ae319b2096bdf651717af5d9d60ed5ef04
kouki-sakata <kouki.sakata.works@gmail.com>
2025-11-20 14:53:27 +0900

    fix: scrool

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import TodoForm from "./components/TodoForm";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full max-w-2xl">
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center w-full max-w-2xl mx-auto">
         <h1 className="text-4xl font-bold text-center w-full">ToDo App</h1>
         <TodoForm />
         <TodoList />
